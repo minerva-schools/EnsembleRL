e52781a354748d7d86b488c485807e5e26622a27
JoeSnow7 <flomay@minerva.kgi.edu>
2020-04-03 23:06:52 -0700

    Add files via upload

---
+++ cartpole_qlearning.py
@@ -0,0 +1,96 @@
+"""
+Base code taken from: 
+https://github.com/IsaacPatole/CartPole-v0-using-Q-learning-SARSA-and-DNN/blob/master/Qlearning_for_cartpole.py
+"""
+
+class CartPoleQAgent():
+    def __init__(self, buckets=(3, 3, 6, 12), num_episodes=500, min_lr=0.1, min_epsilon=0.1, discount=1.0, decay=25):
+        self.buckets = buckets
+        self.num_episodes = num_episodes
+        self.min_lr = min_lr
+        self.min_epsilon = min_epsilon
+        self.discount = discount
+        self.decay = decay
+
+        self.env = gym.make('CartPole-v0')
+
+        # [position, velocity, angle, angular velocity]
+        self.upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50) / 1.]
+        self.lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50) / 1.]
+
+        self.Q_table = np.zeros(self.buckets + (self.env.action_space.n,))
+
+    def discretize_state(self, obs):
+        discretized = list()
+        for i in range(len(obs)):
+            scaling = (obs[i] + abs(self.lower_bounds[i])) / (self.upper_bounds[i] - self.lower_bounds[i])
+            new_obs = int(round((self.buckets[i] - 1) * scaling))
+            new_obs = min(self.buckets[i] - 1, max(0, new_obs))
+            discretized.append(new_obs)
+        return tuple(discretized)
+
+    def choose_action(self, state):
+        if (np.random.random() < self.epsilon):
+            return self.env.action_space.sample() 
+        else:
+            return np.argmax(self.Q_table[state])
+        
+    def get_action(self, state, e):
+        """This method assumes the state fed hasn't been discretized."""
+        obs = discretize_state(state)
+        max_action = np.argmax(self.Q_table[obs])
+        epsilon = self.get_epsilon(e)
+        print(epsilon)
+        action_vector = np.zeros(2)
+        action_vector[max_action-1] = epsilon/2.0
+        action_vector[max_action] = 1-action_vector[max_action-1]
+        return action_vector
+
+    def update_q(self, state, action, reward, new_state):
+        self.Q_table[state][action] += self.learning_rate * (reward + self.discount * np.max(self.Q_table[new_state]) - self.Q_table[state][action])
+
+    def get_epsilon(self, t):
+        return max(self.min_epsilon, min(1., 1. - math.log10((t + 1) / self.decay)))
+
+    def get_learning_rate(self, t):
+        return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
+
+    def train(self):
+        for e in range(self.num_episodes):
+            current_state = self.discretize_state(self.env.reset())
+
+            self.learning_rate = self.get_learning_rate(e)
+            self.epsilon = self.get_epsilon(e)
+            done = False
+            steps = 0
+
+            while not done and steps<200:
+                steps += 1
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.discretize_state(obs)
+                self.update_q(current_state, action, reward, new_state)
+                current_state = new_state
+
+        print('Finished training!')
+
+    def run(self):
+        self.env = gym.wrappers.Monitor(self.env,'cartpole')
+        t = 0
+        done = False
+        current_state = self.discretize_state(self.env.reset())
+        while not done:
+                self.env.render()
+                t = t+1
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.discretize_state(obs)
+                current_state = new_state
+            
+        return t   
+
+
+def load_q_learning():
+    agent = CartPoleQAgent()
+    agent.train()
+    return agent
